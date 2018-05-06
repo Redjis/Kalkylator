205ff8f9f56ba49bc3f61f2dd65a8d4a20376dcb
Redjis <redjis2@mail.ru>
2018-05-06 16:26:48 +0300

    3 проверка

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println ("Hellow world!!!1");
+	System.out.println ("Hellow world!!!12");
     }
 }
