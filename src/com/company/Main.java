d0c9c5a9a94ec295b7974874290559a158aeda6b
Redjis <redjis2@mail.ru>
2018-05-07 00:02:09 +0300

    калькулятор на сложение

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println ("Hellow world!!!12");
+        Scanner sc = new Scanner(System.in); // создаём объект класса Scanner
+        float i;
+        float o;
+        float p;
+        System.out.println("Введите первое число");
+        i = sc.nextFloat();
+        System.out.println("Вы ввели число" + i);
+        System.out.println("Введите второе число");
+        o = sc.nextFloat();
+        System.out.println("Вы ввели число" + o);
+        p=i+o;
+        System.out.println("Сумма равна" +p);
+
     }
 }
